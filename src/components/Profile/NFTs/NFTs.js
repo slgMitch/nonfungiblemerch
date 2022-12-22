0c74b3331bce2f72855df89c819ebe6d22a62c1f
mitchmakos <mitch.makos1@protonmail.com>
2022-12-22 15:57:38 -0500

    more nft

--- src/components/Profile/NFTs/NFTs.js
+++ src/components/Profile/NFTs/NFTs.js
@@ -10,9 +10,11 @@ import {
     CardActions,
     ToggleButton,
     Tooltip,
-    Button
+    Button,
+    Modal
   } from '@mui/material'
 import useSWR from 'swr'
+import axios from 'axios'
 
 export default function NFTs(props) {
     const { user } = props
@@ -20,7 +22,9 @@ export default function NFTs(props) {
     const url = `/api/profile/nfts/${user}`
     const { data, error } = useSWR(url, fetcher)
     const [nfts, setNfts] = useState()
+    const [walletNfts, setWalletNfts] = useState()
     const [isLoading, setIsLoading] = useState(false)
+    const [showNftModal, setShowNftModal] = useState(false)
 
     useEffect(() => {
         console.log('nfts data', data)
@@ -28,6 +32,18 @@ export default function NFTs(props) {
         setNfts(data)
         setIsLoading(false)
     }, [data])
+
+    const displayWalletNfts = async () => {
+        const requestData = { address: user }
+        const { data } = await axios.post('/api/nft/wallet-nfts', requestData, {
+            headers: {
+                'content-type': 'application/json',
+            },
+        });
+        console.log('wallet nfts', data)
+        setWalletNfts(data)
+        setShowNftModal(true)
+    }
 
     if(!data || !nfts || isLoading) {
         return (
@@ -96,10 +112,68 @@ export default function NFTs(props) {
                                         </Typography>
                                     </CardContent>
                                     <CardActions>
-                                        <Button>Add NFTs</Button>
+                                        <Button onClick={() => displayWalletNfts()}>Add NFTs</Button>
                                     </CardActions>
                                 </Card>
                             </Grid>
+                            <Modal
+                                open={showNftModal}
+                            >
+                                <Grid
+                                    container 
+                                    direction="row" 
+                                    justifyContent="center" 
+                                    alignContent="center"
+                                    style={{ minHeight: "100vh" }}
+                                >
+                                    <Grid item xs={8}>
+                                        <Card sx={{ borderRadius: '16px', top: '50%' }}>
+                                            <CardContent>
+                                                <Typography variant='h4' textAlign='center'>Select NFTs to save</Typography>
+                                                <Grid container direction='row'>
+                                                    {
+                                                        walletNfts && walletNfts.map((walletNft) => (
+                                                            <Grid item xs={4}>
+                                                                <Card 
+                                                                    sx={{ 
+                                                                        margin: '15px', 
+                                                                        ":hover": { 
+                                                                            boxShadow: '-1px 10px 29px 0px rgba(0,0,0,0.8);', 
+                                                                            cursor: 'pointer' 
+                                                                        },
+                                                                        
+                                                                        ":onclick": {
+                                                                            boxShadow: '-1px 10px 29px 0px rgba(0,0,0,0.8);', 
+                                                                        }
+                                                                         
+                                                                    }}
+                                                                    // onClick={() => handleAuth('metamask')}
+                                                                >
+                                                                    <CardMedia 
+                                                                        sx={{ height: 350 }}
+                                                                        image={walletNft.metadata.image}
+                                                                        title={walletNft.metadata.name}
+                                                                    />
+                                                                    <CardContent>
+                                                                        <Typography textAlign='center'>{walletNft.metadata.name}</Typography>
+                                                                    </CardContent>
+                                                                </Card>
+                                                            </Grid>
+
+                                                        ))
+                                                    }
+                                                </Grid>
+                                            </CardContent>
+                                            <CardActions>
+                                                <Button onClick={() => setShowNftModal(false)}>Close</Button>
+                                            </CardActions>
+
+                                        </Card>
+                                    </Grid>
+
+                                </Grid>
+
+                            </Modal>
 
                         </Grid>
                     )
