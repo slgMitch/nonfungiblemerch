a5e04dce883c4d5476ac7ef3d0666614de2ea56d
mitchmakos <mitch.makos1@protonmail.com>
2023-02-01 10:41:00 -0500

    updating checkout page

--- pages/product/[syncProductId]/variant/[syncVariantId].js
+++ pages/product/[syncProductId]/variant/[syncVariantId].js
@@ -21,7 +21,87 @@ export default function CustomVariant(props) {
         container
         direction='row'
     >
-        <Typography>Custom Variant by External Id</Typography>
+        <Grid item xs={6} align="center">
+          <Image 
+            src={variant.imagePreviewUrl}
+            alt={variant.syncVariantId}
+            width={500}
+            height={500}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography align="center" variant="h4" gutterBottom>{variant.name}</Typography>
+          <br/>
+          <Typography variant="h5"> Price: ${variant.retailPrice} </Typography>
+          <br/>
+          <Grid item xs={12}>
+            <Grid container direction="row">
+              <Grid item xs={6}>
+                <FormControl sx={{ m: 1, minWidth: 420 }}>
+                <InputLabel id="product-color-select-label">Color</InputLabel>
+                  <Select
+                    labelId="product-color-select-label"
+                    id="product-color-select"
+                    value={variant.color.color}
+                    label="Color"
+                    // onChange={changeSelectedColor}
+                  >
+                    {
+                        products &&
+                        products.map((product) => (
+                            <MenuItem key={product.color.color} value={product.color.color}>{product.color.color}</MenuItem>
+                        ))
+                    }
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <FormControl sx={{ m: 1, minWidth: 420 }}>
+                  <InputLabel id="product-size-select-label">Size</InputLabel>
+                  <Select
+                      labelId="product-size-select-label"
+                      id="product-size-select"
+                      value={variant.size}
+                      label="Size"
+                      // onChange={changeSelectedSize}
+                  >
+                      {
+                          products &&
+                          products.map((product) => (
+                              <MenuItem key={product.size} value={product.size}>{product.size}</MenuItem>
+                          ))
+                      }
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                  <br />
+                  <Typography variant="h5">Status: {variant.availabilityStatus[0].status === 'in_stock' ? 'In Stock' : 'Out of Stock'}  </Typography>
+              </Grid>
+              <Grid item align="center" xs={12}>
+                  <br />
+                  <Button 
+                      variant="contained" 
+                      size="large" 
+                      sx={{
+                          minWidth: 800
+                      }}
+                      disabled={variant.availabilityStatus[0].status === 'in_stock' ? false : true}
+                      className="snipcart-add-item"
+                      data-item-id={variant.syncVariantId}
+                      data-item-price={variant.retailPrice}
+                      data-item-url={`/product/${variant.syncProductId}/variant/${variant.syncVariantId}`}
+                      data-item-name={variant.name}
+                      data-item-image={variant.imagePreviewUrl}
+                  >
+                      Add to Cart
+                  </Button>
+              </Grid>
+
+            </Grid>
+          </Grid>
+
+        </Grid>
 
     </Grid>
     )
@@ -31,14 +111,14 @@ export default function CustomVariant(props) {
 
 export async function getServerSideProps(context) {
   const { syncProductId, syncVariantId } = context.params;
-  console.log("{ syncProductId, syncVariantId }", { syncProductId, syncVariantId });
+  // console.log("{ syncProductId, syncVariantId }", { syncProductId, syncVariantId });
   const response = await fetch(`http://localhost:3000/api/product/${syncProductId}/variant/${syncVariantId}`);
   const { products, variant } = await response.json()
-  console.log('getServerSideProps data', { products, variant });
+  // console.log('getServerSideProps data', { products, variant });
   return {
     props: {
         products,
-        variant
+        variant: variant[0]
     }
   }
 }
