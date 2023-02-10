df2dd3374196bea41643de26d699c3023fee8eb2
mitchmakos <mitch.makos1@protonmail.com>
2023-02-10 11:54:48 -0500

    seeing auth req data

--- pages/api/auth/request-message.js
+++ pages/api/auth/request-message.js
@@ -9,10 +9,14 @@ export default async function handler(req, res) {
             domain: process.env.APP_DOMAIN,
             statement: 'Please sign this message to confirm your identity.',
             uri: process.env.NEXTAUTH_URL,
-            timeout: 60
+            timeout: 60,
+            key: process.env.MORALIS_API_KEY,
+            address,
+            chain,
+            network
         };
         
-        await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
+        // await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
         
         // const message = await Moralis.Auth.requestMessage({
         //     address,
