8551bf018289b22ac2d8ba263e1e348c4f9c1740
mitchmakos <mitch.makos1@protonmail.com>
2023-02-16 16:27:38 -0500

    hitting endpoint

--- pages/api/auth/request-message.js
+++ pages/api/auth/request-message.js
@@ -5,31 +5,31 @@ export default async function handler(req, res) {
     try {
         const { address, chain, network } = JSON.parse(req.body);
     
-        const config = {
-            domain: process.env.APP_DOMAIN,
-            statement: 'Please sign this message to confirm your identity.',
-            uri: process.env.NEXTAUTH_URL,
-            timeout: 60,
-        };
+        // const config = {
+        //     domain: process.env.APP_DOMAIN,
+        //     statement: 'Please sign this message to confirm your identity.',
+        //     uri: process.env.NEXTAUTH_URL,
+        //     timeout: 60,
+        // };
         
-        await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
+        // await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
         
-        const message = await Moralis.Auth.requestMessage({
-            address,
-            chain,
-            network,
-            ...config
-        });
+        // const message = await Moralis.Auth.requestMessage({
+        //     address,
+        //     chain,
+        //     network,
+        //     ...config
+        // });
 
 
-        const client = await connectDatabase()
-        const existingUser = await getOneDocument(client, 'users', { user: address })
+        // const client = await connectDatabase()
+        // const existingUser = await getOneDocument(client, 'users', { user: address })
 
-        if(!existingUser) {
-            await insertDocument(client, 'users', { user: address })
-        }
+        // if(!existingUser) {
+        //     await insertDocument(client, 'users', { user: address })
+        // }
 
-        res.status(200).json(message);
+        res.status(200).json({ address, chain, network });
     } catch (error) {
         console.log('there was an error in request message', error)
         res.status(400).json(error);
